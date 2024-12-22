c8da178c83fadba7e0d3ee9abfe2bb2ba39a432f
mrjulien44 <julientheot@hotmail.com>
2024-12-23 00:01:48 +0100

    gpt 1.1

--- custom_components/hacs-heitzfit4/__init__.py
+++ custom_components/hacs-heitzfit4/__init__.py
@@ -1,19 +1,60 @@
-from homeassistant.helpers import discovery
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+# from homeassistant.helpers import discovery
+# from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 
-DOMAIN = "hacs-heitzfit4"
+# DOMAIN = "hacs-heitzfit4"
 
-def setup(hass, config):
-    conf = config[DOMAIN]
-    username = conf[CONF_USERNAME]
-    password = conf[CONF_PASSWORD]
-    club = conf["club"]
+# def setup(hass, config):
+#     conf = config[DOMAIN]
+#     username = conf[CONF_USERNAME]
+#     password = conf[CONF_PASSWORD]
+#     club = conf["club"]
+
+#     hass.data[DOMAIN] = {
+#         "username": username,
+#         "password": password,
+#         "club": club
+#     }
+
+#     discovery.load_platform(hass, "sensor", DOMAIN, {}, config)
+#     return True
+
+
+"""Initialisation du package de l'intégration """
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+from .const import DOMAIN, PLATFORMS
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(
+    hass: HomeAssistant, config: ConfigEntry
+):  # pylint: disable=unused-argument
+    """Initialisation de l'intégration"""
+    _LOGGER.info(
+        "Initializing %s integration with plaforms: %s with config: %s",
+        DOMAIN,
+        PLATFORMS,
+        config,
+    )
+
+    # Mettre ici un eventuel code permettant l'initialisation de l'intégration
+    # Ca peut être une connexion sur le Cloud qui fournit les données par ex
+    # (pas nécessaire pour le tuto)
+
+    # L'argument config contient votre fichier configuration.yaml
+    my_config = config.get(DOMAIN)  # pylint: disable=unused-variable
+    username = my_config[CONF_USERNAME]
+    password = my_config[CONF_PASSWORD]
+    club = my_config["club"]
 
     hass.data[DOMAIN] = {
         "username": username,
         "password": password,
         "club": club
     }
-
-    discovery.load_platform(hass, "sensor", DOMAIN, {}, config)
+    # Return boolean to indicate that initialization was successful.
     return True
